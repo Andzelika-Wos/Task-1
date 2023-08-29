cc4001a34643000ea479839d4ea1e85c1a6bbf85
Anda <andzelikawos56@gmail.com>
2023-08-29 14:56:29 +0200

    midway through, strugling with true/false

--- Task 1/Variables and if-else.cpp
+++ Task 1/Variables and if-else.cpp
@@ -5,23 +5,73 @@ int main()
 {
 	int money;
 	money = 0;
-	bool spend;
-	spend = true;
+	char spend;
+	spend = 'y';
+	char notSpend;
+	notSpend = 'n';
+	bool spend1 = false;
+	bool notSpend1 = false;
 
 	if (money == 0)
 	{
 		cout << "You're broke as fuck\n";
-		cout << "Gimme money\n";
+		cout << "Gimme money -_-\n";
+		cout << "Enter amount:\n";
 		cin >> money;
+		if (money <= 0)
+		{
+			cout << "that's not possible silly :*\n";
+			cout << "silly goose\n";
+
+			return 0;
+		}
 		cout << "Money is " << money << "\n";
-	}
-	else (money != 0)
-	{
 		cout << "Spend?\n";
+		cout << "y or n\n";
 		cin >> spend;
-
+		if
+		if (spend1 == true)
+		{
+			cout << "How much?\n";
+			cin >> spend;
+			cout << "Now u broke again lol\n";
+		}
+		if (notSpend1 == false)
+		{
+			cout << "Wise choice :3\n";
+		}
+		
+		
+		if (spend == 'y')
+		{
+			cout << " ";
+		}
+		else
+		{
+			if (notSpend == 'n')
+			{
+				cout << 'n';
+			}
+		}
+		//cout << !((spend == 'y') || (notSpend == 'n')) << "\n";
 	}
-
+	else if (money != 0){
+		cout << "Money is " << money << "\n";
+		cout << "Spend?\n";
+		cout << "y or n\n";
+		cin >> spend;
+		cout << ((spend == 'y') || (notSpend == 'n')) << "\n";
+	}
+	if(spend == 'y')
+	{
+		cout << "How much?\n";
+		cin >> spend;
+		cout << "Now u broke again lol\n";
+	}
+	else if (spend == 'n')
+	{
+		cout << "Wise choice :3\n";
+	}
 
 	return 0;
 }
