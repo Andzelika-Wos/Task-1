4d510c29a91c1be49a6260323595b60d07cc721a
Anda <andzelikawos56@gmail.com>
2023-08-29 12:06:42 +0200

    if statment done?

--- Task 1/Variables and if-else.cpp
+++ Task 1/Variables and if-else.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
+	int money;
+	money = 0;
+	bool spend;
+	spend = true;
 
+	if (money == 0)
+	{
+		cout << "You're broke as fuck\n";
+		cout << "Gimme money\n";
+		cin >> money;
+		cout << "Money is " << money << "\n";
+	}
+	else (money != 0)
+	{
+		cout << "Spend?\n";
+		cin >> spend;
+
+	}
+
+
+	return 0;
 }
